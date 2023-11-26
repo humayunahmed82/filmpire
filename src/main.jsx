dce7765569148e438a2d8b786e7617717c72d65c
Humayun Ahmed <40520080+humayunahmed82@users.noreply.github.com>
2023-11-26 14:01:08 +0600

    The Navigation Bar has been successfully completed.
    The Navigation Bar has been successfully completed.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import App from "./App.jsx";
 
+const theme = createTheme({});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
+	<ThemeProvider theme={theme}>
+		<React.StrictMode>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</React.StrictMode>
+	</ThemeProvider>
 );
