bac73f4e0c768762464a2239aa8472af7b1d6e22
Humayun Ahmed <40520080+humayunahmed82@users.noreply.github.com>
2023-12-04 23:30:51 +0600

    Authentication has been completed successfully.
    I have successfully completed the authentication process.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,37 @@
+import { useSelector } from "react-redux";
+import { userSelector } from "../../features/auth";
+import { Box, Button, Typography } from "@mui/material";
+
 const Profile = () => {
-	return <h1>Profile</h1>;
+	const { user } = useSelector(userSelector);
+
+	const favoriteMovies = [];
+
+	const logout = () => {
+		localStorage.clear();
+
+		window.location.href = "/";
+	};
+
+	return (
+		<Box>
+			<Box display="flex" justifyContent="space-between">
+				<Typography variant="h4" gutterBottom>
+					My Profile
+				</Typography>
+				<Button color="inherit" onClick={logout}>
+					Logout &nbsp;
+				</Button>
+			</Box>
+			{!favoriteMovies.length ? (
+				<Typography variant="h5">
+					Add Favorite or watchlist some to see them here!
+				</Typography>
+			) : (
+				<Box>FAVORITE MOVIES</Box>
+			)}
+		</Box>
+	);
 };
 
 export default Profile;
