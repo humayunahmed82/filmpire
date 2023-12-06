2298798b6264e4bb4fc1bb1364df426dc7c441ca
Humayun Ahmed <40520080+humayunahmed82@users.noreply.github.com>
2023-12-07 00:41:37 +0600

    Movie Information Page - Part 1
    Movie Information Page - Part 1

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,12 +1,18 @@
 import { Link, useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
 import { useGetMovieQuery } from "../../services/TMBD";
+import { selectGenreOrCategory } from "../../features/currentGenreOrCategory";
+
 import { Box, CircularProgress, Grid, Rating, Typography } from "@mui/material";
+import genresIcon from "../../assets/genres/index";
 
 import useStyle from "./styles";
 
 const MovieInformation = () => {
 	const { id } = useParams();
 	const { data, isFetching, error } = useGetMovieQuery(id);
+	const dispatch = useDispatch();
 
 	const classes = useStyle();
 
@@ -47,7 +53,40 @@ const MovieInformation = () => {
 				<Grid container className={classes.containerSpaceAround}>
 					<Box display="flex" align="center">
 						<Rating readOnly value={data?.vote_average / 2} />
+						<Typography
+							variant="subtitle1"
+							gutterBottom
+							style={{ marginLeft: "10px" }}
+						>
+							{data?.vote_average} / 10
+						</Typography>
 					</Box>
+					<Typography variant="h6" align="center" gutterBottom>
+						{data?.runtime}min /{" "}
+						{data?.spoken_languages.length > 0
+							? `${data?.spoken_languages[0].name}`
+							: ""}
+					</Typography>
+				</Grid>
+				<Grid item className={classes.genresContainer}>
+					{data?.genres.map((genre, index) => (
+						<Link
+							className={classes.links}
+							to="/"
+							key={genre.name}
+							onClick={() => dispatch(selectGenreOrCategory(genre.id))}
+						>
+							<img
+								src={genresIcon[genre.name.toLowerCase()]}
+								className={classes.genreImages}
+								height={30}
+								alt=""
+							/>
+							<Typography variant="subtitle1" color="textPrimary">
+								{genre.name}
+							</Typography>
+						</Link>
+					))}
 				</Grid>
 			</Grid>
 		</Grid>
--- src/components/MovieInformation/styles.js
+++ src/components/MovieInformation/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@mui/styles";
 export default makeStyles((theme) => ({
 	containerSpaceAround: {
 		display: "flex",
-		justifyContent: "center",
+		justifyContent: "space-around",
 		margin: "10px 0 !important",
 		[theme.breakpoints.down("sm")]: {
 			flexDirection: "column",
@@ -14,16 +14,34 @@ export default makeStyles((theme) => ({
 		borderRadius: "20px",
 		boxShadow: "0.5em 1em 1em rgba(64,64,70)",
 		width: "80%",
+		[theme.breakpoints.down("md")]: {
+			margin: "0 auto",
+			width: "50%",
+			height: "350px",
+		},
 		[theme.breakpoints.down("sm")]: {
 			margin: "0 auto",
 			width: "100%",
 			height: "350px",
 			marginBottom: "30px",
 		},
-		[theme.breakpoints.down("md")]: {
-			margin: "0 auto",
-			width: "100%",
-			height: "350px",
+	},
+	genresContainer: {
+		margin: "10px 0 !important",
+		display: "flex",
+		justifyContent: "space-around",
+		flexWrap: "wrap",
+	},
+	genreImages: {
+		filter: theme.palette.mode === "dark" && "invert(1)",
+		marginRight: "10px",
+	},
+	links: {
+		display: "flex",
+		alignItems: "center",
+		alignItems: "center",
+		[theme.breakpoints.down("sm")]: {
+			padding: "0.5rem 1rem",
 		},
 	},
 }));
